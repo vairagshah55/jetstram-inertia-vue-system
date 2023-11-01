b73d3fbfedb3edfd81b0f44273762bd3b4ddeae4
vairags <vairags@universal-software.com>
2023-11-01 23:15:50 +0530

    dashboard display controller code

---
+++ app/Http/Controllers/Web/BookingContoller.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers\Web;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\EditProductRequest;
+use App\Http\Requests\StoreProductRequest;
+use App\Models\Product;
+use Illuminate\Support\Facades\Redirect;
+use Illuminate\Support\Facades\Request;
+use Illuminate\Support\Facades\URL;
+use Inertia\Inertia;
+
+class BookingContoller extends Controller
+{
+    public function dashboard(Request $request)
+    {
+        return Inertia::render('Dashboard');
+    }
+}
